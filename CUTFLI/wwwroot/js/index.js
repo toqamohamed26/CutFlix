e3203a28336fd06e7381aa2e47190208362a9b48
toqa mohamed <toqamohamed102@gmail.com>
2023-10-20 23:02:28 +0300

    modify

--- CUTFLI/wwwroot/js/index.js
+++ CUTFLI/wwwroot/js/index.js
@@ -1,2 +1,10 @@
-const video = document.getElementById('myVideo');
-video.play();
+var video = document.getElementById("myVideo");
+var playButton = document.querySelector(".play");
+
+playButton.addEventListener("click", function () {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
