0513e937a7d065bd6653ab21851ee1d90622abd9
toqa mohamed <toqamohamed102@gmail.com>
2023-10-21 01:16:34 +0300

    add

--- CUTFLI/wwwroot/js/index.js
+++ CUTFLI/wwwroot/js/index.js
@@ -1,10 +1,22 @@
 var video = document.getElementById("myVideo");
-var playButton = document.querySelector(".play");
+var playButton = document.getElementById("playButton");
 
 playButton.addEventListener("click", function () {
     if (video.paused) {
         video.play();
+        playButton.style.display = "none";
     } else {
         video.pause();
+        playButton.style.display = "block";
+    }
+});
+
+video.addEventListener("click", function () {
+    if (video.paused) {
+        video.play();
+        playButton.style.display = "none";
+    } else {
+        video.pause();
+        playButton.style.display = "block";
     }
 });
